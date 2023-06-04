64c38f2aca0c4ed7d27841484c5b824a4e33a8f7
Weten-Ogen <alphaoware@gmail.com>
2023-06-04 17:13:36 +0000

    fixed the second bug on the app.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { NewsArticle, NewsResponse } from '@/models/NewsArticles';
 
 
 
-export async function HomePage() {
+export async function HomePage():Promise<NewsArticle> {
   await new Promise(r => setTimeout(r,6000));
   const response =  await fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=" + process.env.Api_Key);
 

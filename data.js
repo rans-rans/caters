9aa060ef9c3ff5cfb0e2c7b23ed021aba77d0b5e
Weten-Ogen <alphaoware@gmail.com>
2023-10-25 03:34:17 +0000

    first commit

--- data.js
+++ data.js
@@ -1,7 +1,126 @@
-export const data = [
-    {
-        name:"Python",
-        author:"Von",
-        birth:"1996",
-    }
-]
+export const data = {
+    "programming_languages": [
+      {
+        "name": "Python",
+        "year_created": 1991,
+        "creator": "Guido van Rossum",
+        "users": 1000000
+      },
+      {
+        "name": "Java",
+        "year_created": 1995,
+        "creator": "James Gosling",
+        "users": 9000000
+      },
+      {
+        "name": "C++",
+        "year_created": 1983,
+        "creator": "Bjarne Stroustrup",
+        "users": 5000000
+      },
+      {
+        "name": "JavaScript",
+        "year_created": 1995,
+        "creator": "Brendan Eich",
+        "users": 8000000
+      },
+         
+          {
+            "name": "C#",
+            "year_created": 2000,
+            "creator": "Microsoft",
+            "users": 4000000
+          },
+          {
+            "name": "Swift",
+            "year_created": 2014,
+            "creator": "Apple",
+            "users": 500000
+          },
+          {
+            "name": "Go",
+            "year_created": 2009,
+            "creator": "Robert Griesemer, Rob Pike, Ken Thompson",
+            "users": 1000000
+          },
+          {
+            "name": "Ruby",
+            "year_created": 1995,
+            "creator": "Yukihiro Matsumoto",
+            "users": 300000
+          },
+          {
+            "name": "Rust",
+            "year_created": 2010,
+            "creator": "Mozilla",
+            "users": 500000
+          },
+          {
+            "name": "Kotlin",
+            "year_created": 2011,
+            "creator": "JetBrains",
+            "users": 200000
+          },
+          {
+            "name": "TypeScript",
+            "year_created": 2012,
+            "creator": "Microsoft",
+            "users": 400000
+          },
+          {
+            "name": "Dart",
+            "year_created": 2011,
+            "creator": "Google",
+            "users": 300000
+          },
+          {
+            "name": "Haskell",
+            "year_created": 1990,
+            "creator": "Simon Peyton Jones",
+            "users": 100000
+          },
+          {
+            "name": "Elixir",
+            "year_created": 2011,
+            "creator": "José Valim",
+            "users": 10000
+          },
+          {
+            "name": "Scala",
+            "year_created": 2003,
+            "creator": "Martin Odersky",
+            "users": 200000
+          },
+          {
+            "name": "R",
+            "year_created": 1993,
+            "creator": "Ross Ihaka and Robert Gentleman",
+            "users": 500000
+          },
+          {
+            "name": "Perl",
+            "year_created": 1987,
+            "creator": "Larry Wall",
+            "users": 200000
+          },
+          {
+            "name": "Lua",
+            "year_created": 1993,
+            "creator": "Roberto Ierusalimschy",
+            "users": 100000
+          },
+          {
+            "name": "Erlang",
+            "year_created": 1986,
+            "creator": "Ericsson",
+            "users": 50000
+          },
+          {
+            "name": "Crystal",
+            "year_created": 2011,
+            "creator": "Ary Borenszweig, Juan Wajnerman",
+            "users": 10000
+          }
+        ]
+  }
+  

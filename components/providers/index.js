08f7c8cee169177167caa0424f7e1778be370b95
Weten-Ogen <alphaoware@gmail.com>
2024-08-09 02:00:39 +0000

    first commit

--- components/providers/index.js
+++ components/providers/index.js
@@ -5,7 +5,7 @@ import NavBar from '../ui/navbar'
 export default function Providers({children}) {
   return (
     <div className="">
-        <main className=" w-full md:max-w-[80%] mx-auto py-5 p-8 md:p-0 bg-p-white">
+        <main className=" w-full md:max-w-[80%] mx-auto py-5 p-4 md:p-0 bg-p-white">
             <NavBar/> 
             <div className="py-16">
             {children}

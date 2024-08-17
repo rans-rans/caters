1600dfc1601870b9f058d0f40be1d303c1bd5702
Weten-Ogen <alphaoware@gmail.com>
2024-08-17 13:02:59 +0000

    first commit

--- types/index.d.ts
+++ types/index.d.ts
@@ -26,20 +26,11 @@ declare type LoginUser = {
 };
 
 declare type User = {
-  $id: string;
   email: string;
-  userId: string;
-  dwollaCustomerUrl: string;
-  dwollaCustomerId: string;
   firstName: string;
   lastName: string;
-  name: string;
-  address1: string;
-  city: string;
-  state: string;
-  postalCode: string;
-  dateOfBirth: string;
-  ssn: string;
+  
+  
 };
 
 declare type NewUserParams = {
